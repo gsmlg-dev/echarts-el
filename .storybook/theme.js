5bd9109970abc07c6310f21cc47e4485a259a016
Jonathan Gao <gsmlg.com@gmail.com>
2022-04-24 20:28:39 +0800

    docs: Fix banner url.

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,6 +2,6 @@ import { create } from '@storybook/theming';
 
 export default create({
   brandTitle: 'echarts-el',
-  brandUrl: 'https://gsmlg-dev.github.io/echarts-el',
+  brandUrl: 'https://github.com/gsmlg-dev/echarts-el',
   brandImage: 'https://gsmlg.github.io/svg/gsmlg-dev.svg',
 });
